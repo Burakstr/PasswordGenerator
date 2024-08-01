9b46f2541fc09fd4d2d9ecd603c8218ea915d65a
Burak <145280474+Burakstr@users.noreply.github.com>
2024-08-01 09:37:04 +0300

    Add files via upload

---
+++ PasswordGenerator/PasswordGenerator/PasswordGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorApp.swift
+//  PasswordGenerator
+//
+//  Created by Burak Satır on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
